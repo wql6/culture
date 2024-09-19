930ff5d44c5898eb8123637ff14e64f3b8ddc1e8
wuql <wuql>
2024-09-19 16:33:14 +0800

    删除Note

--- server/api/articleContent.ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler } from 'h3'
-// @ts-ignore
-import { getContent } from '@nuxt/content'
-
-export default defineEventHandler(async () => {
-    const content = await getContent('articles/example').fetch()
-    return content.body
-})
